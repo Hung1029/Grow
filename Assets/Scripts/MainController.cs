b61b8e29bc1585e0ce86e308f08fe80855290fcf
Hung1029 <39110096+Hung1029@users.noreply.github.com>
2023-12-02 21:50:52 +0800

    1202_ToBird

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainController : MonoBehaviour
@@ -56,7 +57,10 @@ public class MainController : MonoBehaviour
     public void EnterStageClick(int num)
     {
         ButtonClickAudioPlay();
-        Debug.Log("EnterStage: "+num);
+        //Debug.Log("EnterStage: "+num);
+
+        SceneManager.LoadScene(num);
+
     }
 
     public void ButtonClickAudioPlay()
